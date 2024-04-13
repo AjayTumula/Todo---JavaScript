b49a2efefd5d826f48d269aea34846cc529384e5
AjayTumula <ajaytumula72@gmail.com>
2024-04-13 18:19:05 +0100

    first commit for Todo application

---
+++ Todo Application/index.js
@@ -0,0 +1,47 @@
+
+
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+let msg = document.getElementById('msg');
+let posts = document.getElementById('posts');
+
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    console.log('button clicked');
+    formValidation();
+})
+
+let formValidation = () => {
+    if(input.value === ""){
+        msg.innerHTML = "Todo can't be blank";
+        console.log("failed");
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+        createPosts();
+    }
+}
+
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+} 
+
+
+let createPosts = () => {
+    posts.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+        <span class="options">
+            <i onClick="editPost(this)" class="fas fa-edit"></i>
+            <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+    </div>
+`;
+input.value ="";
+}
+
